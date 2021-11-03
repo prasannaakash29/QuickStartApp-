5c983e690519b7388d92817d83e76069bab3a3a2
lmkwytnicholas <nicholas@lmkwyt.org.com>
2021-11-04 02:04:07 +0530

    Changes done

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 const LOGIN_USER_KEY = 'WD_FORUM_LOGIN_USER_KEY';
 
 var baseURL;
-if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
-    baseURL = process.env.REACT_APP_API_BASE_URL;
-} else {
-    baseURL = 'http://127.0.0.1:8000';
-}
+// if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
+//     baseURL = process.env.REACT_APP_API_BASE_URL;
+// } else {
+//     baseURL = 'http://127.0.0.1:8000';
+// }
 
-const api = axios.create({
+baseURL="https://backend-prasanna.herokuapp.com/" 
+ const api = axios.create({
     baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json'
