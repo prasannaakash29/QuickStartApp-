4a27e24b135616b5cd7e8c370ff8f0110559c944
lmkwytnicholas <nicholas@lmkwyt.org.com>
2021-11-25 03:12:14 +0530

    api

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -2,13 +2,13 @@ import axios from "axios";
 const LOGIN_USER_KEY = "LOGIN_USER_KEY";
 
 var baseURL;
-if (
-  process.env.REACT_APP_ENVIRONMENT &&
-  process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
-) {
-  baseURL = process.env.REACT_APP_API_BASE_URL;
-} else 
-  baseURL = "http://127.0.0.1:8000";
+// if (
+//   process.env.REACT_APP_ENVIRONMENT &&
+//   process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
+// ) {
+//   baseURL = process.env.REACT_APP_API_BASE_URL;
+// } else 
+  baseURL = "https://frontend-prasanna.herokuapp.com/";
 
 // baseURL = "https://fresh-zest-backend-a.herokuapp.com/";
 
